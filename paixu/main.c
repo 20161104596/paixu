92e2185e37ec4f19b5fc4ec9bf9200fa3248280c
20161104596 <964312086@qq.com>
2017-06-12 11:47:36 +0800

    数字排序

--- paixu/main.c
+++ paixu/main.c
@@ -7,9 +7,31 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int a[10];
+    int i,j;
+    int tem;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<10;j++)
+    {
+        for(i=0;i<=10-j;i++)
+        {
+            if(a[i]<a[i+1])
+            {
+                tem=a[i];
+                a[i]=a[i+1];
+                a[i+1]=tem;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d",a[i]);
+        printf("\n");
+    }
     return 0;
 }
