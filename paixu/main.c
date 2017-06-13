8527fe5cbfe36d912cea15056dfd424bed9922f0
20161104596 <964312086@qq.com>
2017-06-13 09:50:36 +0800

    排序

--- paixu/main.c
+++ paixu/main.c
@@ -10,28 +10,38 @@
 int main()
 {
     int a[10];
-    int i,j;
-    int tem;
-    for(i=0;i<10;i++)
+    int i,j,tem;
+    FILE*fp1,*fp2;
+    fp1=fopen("／/Users/／a20161104596/／Desktop/／wenjir／／input.txt","w+");
+    fp2=fopen("／/Users/／a20161104596/／Desktop/／wenjir／／output.txt","r+");
+    if(fp2=NULL)
     {
-        scanf("%d",&a[i]);
+        printf("打开文件错误，打开的文件可能不存在。")
     }
-    for(j=0;j<10;j++)
+    else
     {
-        for(i=0;i<=10-j;i++)
-        {
-            if(a[i]<a[i+1])
-            {
-                tem=a[i];
-                a[i]=a[i+1];
-                a[i+1]=tem;
-            }
-        }
-    }
-    for(i=0;i<10;i++)
-    {
-        printf("%d",a[i]);
-        printf("\n");
+        
+       for(i=0;i<10;i++)
+       {
+           scanf("%d",&a[i]);
+       }
+       for(j=0;j<10;j++)
+       {
+           for(i=0;i<=10-j;i++)
+           {
+               if(a[i]<a[i+1])
+               {
+                   tem=a[i];
+                   a[i]=a[i+1];
+                   a[i+1]=tem;
+               }
+           }
+       }
+       for(i=0;i<10;i++)
+       {
+           printf("%d",a[i]);
+           printf("\n");
+       }
     }
     return 0;
 }
