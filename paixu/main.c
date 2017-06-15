d6e214e2255c8b3fcda166064f2b8134800dbe8c
20161104596 <964312086@qq.com>
2017-06-15 16:11:11 +0800

    排序
    文件中输入输出

--- paixu/main.c
+++ paixu/main.c
@@ -9,39 +9,48 @@
 #include <stdio.h>
 int main()
 {
-    int a[10];
-    int i,j,tem;
-    FILE*fp1,*fp2;
-    fp1=fopen("／/Users/／a20161104596/／Desktop/／wenjir／／input.txt","w+");
-    fp2=fopen("／/Users/／a20161104596/／Desktop/／wenjir／／output.txt","r+");
-    if(fp2=NULL)
+    FILE *fp1;
+    FILE *fp2;
+    fp1=fopen("//Users//a20161104596//Desktop//paixu//input.txt","r");
+    fp2=fopen("//Users//a20161104596//Desktop//paixu//output.txt","w");
+
+    int a[15],i,j,tem;
+    if(fp1==NULL)
     {
-        printf("打开文件错误，打开的文件可能不存在。")
+        printf("文件不存在");
     }
     else
     {
-        
-       for(i=0;i<10;i++)
-       {
-           scanf("%d",&a[i]);
-       }
-       for(j=0;j<10;j++)
-       {
-           for(i=0;i<=10-j;i++)
-           {
-               if(a[i]<a[i+1])
-               {
-                   tem=a[i];
-                   a[i]=a[i+1];
-                   a[i+1]=tem;
-               }
-           }
-       }
-       for(i=0;i<10;i++)
-       {
-           printf("%d",a[i]);
-           printf("\n");
-       }
+        for(i=0; i<10; i++)
+        {
+            fscanf(fp1,"%d",&a[i]);
+        }
+        for(j=0; j<9; j++)
+        {
+            for(i=0; i<9-j; i++)
+            {
+                if(a[i]>a[i+1])
+                {
+                    tem=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=tem;
+                }
+            }
+        }
+        for(i=0; i<10; i++)
+            
+        {
+            fprintf(fp2,"%d ",a[i]);
+            
+            printf("%d ",a[i]);
+            
+        }
+        fprintf(fp2,"\n");
+        printf("\n");
+        fclose(fp1);
+        fclose(fp2);
     }
+    
+    
     return 0;
 }
